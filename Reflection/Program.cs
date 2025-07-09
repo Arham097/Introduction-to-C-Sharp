8635ae1ede3f8d251a63d1cbe03e41dc7780e487
Arham097 <arhamhasan70@gmail.com>
2025-07-09 23:44:07 +0500

    [FEAT]: Reflection added

---
+++ Reflection/Program.cs
@@ -0,0 +1,42 @@
+﻿// Reflection (Dynamically inspect and execute your code)
+// Reflection is used to get information about assemblies, classes, methods, and properties at runtime, and even invoke them dynamically.
+
+using System;
+using System.Reflection;
+class Program
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public string Health;
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Player p1 = new Player();
+        p1.Name = "Arham";
+        p1.SayHello();
+
+        // Get Type Information using reflection
+        Type type = p1.GetType();
+        Console.WriteLine("Class Type: " + type);
+
+        // List all properties using reflection
+        foreach (PropertyInfo prop in typeof(Player).GetProperties())
+        {
+            Console.WriteLine(prop);
+        }
+
+        // List all Methods 
+        Console.WriteLine("Methods: \n");
+        foreach (MethodInfo metInfo in typeof(Player).GetMethods())
+        {
+            Console.WriteLine(metInfo);
+        }
+    }
+
+}
