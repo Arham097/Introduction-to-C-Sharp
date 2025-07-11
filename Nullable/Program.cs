6aa58fb439b0bc42773ce8932089e3bdbe5408e0
Arham097 <arhamhasan70@gmail.com>
2025-07-11 23:46:09 +0500

    [FEAT]: Nullable and Span Added

---
+++ Nullable/Program.cs
@@ -0,0 +1,31 @@
+﻿// Nullble (Types that can handle/store null values)
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // Throw error (Cannot convert null to int)
+        // int i = null;
+        // Console.WriteLine(i);
+        // To make it nullable add ? after type
+        int? i = null;
+        Console.WriteLine(i);
+        bool? b = null;
+        Console.WriteLine(b);
+
+        // THis does not throw error because class on default value is null;
+        Player player = null;
+
+        // or we can make it through Nullable
+        Nullable<int> j = null;
+        if (!j.HasValue) // Check if it is null or not
+        {
+            j = 10;
+        }
+        Console.WriteLine(j);
+    }
+
+    class Player
+    {
+
+    }
+}
---
+++ Span/Program.cs
@@ -0,0 +1,21 @@
+﻿// Span (Super Efficient way to deal with collections without memory cost)
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] numArray = new int[] { 1, 4, 3, 2, 7, 5, 6, 8 };
+        // It does not create new type or takes memory but it points to same memory location of numArray
+        Span<int> numSpan = new Span<int>(numArray, 1, 6);
+        foreach (int num in numSpan)
+        {
+            Console.WriteLine(num);
+        }
+        numSpan.Sort();
+        foreach (int num in numSpan)
+        {
+            Console.WriteLine(num);
+        }
+
+    }
+}
