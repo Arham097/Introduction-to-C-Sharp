81d3741e3e73f6196aabe4fb4ccae725b73e00c6
Arham097 <arhamhasan70@gmail.com>
2025-07-13 11:55:31 +0500

    [FEAT]: Asynchronous Programming, and Multithreading added (C# Tutorial Now Done)

---
+++ Asynchronous Programming/Program.cs
@@ -0,0 +1,59 @@
+﻿// Asynchronous Programming
+// Async Await Task (Run logic without blocking main thread)
+
+using System.Threading.Tasks;
+
+class Program
+{
+    public static string serverData;
+
+    public static async Task Main(string[] args)
+    {
+
+        // await ContactingWebServer();
+        // int dataLength = serverData.Length;
+        // Console.WriteLine(dataLength);
+
+        // This is in sequential manner and one task end then another start but normally these things can be done side by side
+        // await CookEggs();
+        // await CookChicken();
+        // await MakeToast();
+
+        // Another bettter approach this is much faster than above it starts all the task at once and waiting for all to complete
+        Task eggTask = CookEggs();
+        Task chickenTask = CookChicken();
+        Task toastTask = MakeToast();
+
+        await Task.WhenAll(eggTask, chickenTask, toastTask);
+        Console.WriteLine("Breakfast Done");
+    }
+    public static async Task ContactingWebServer()
+    {
+        Console.WriteLine("Contacting Server...");
+        await Task.Delay(3000);
+        serverData = "Data";
+        Console.WriteLine("Got Response");
+    }
+
+    public static async Task CookEggs()
+    {
+        Console.WriteLine("Cooking Eggs");
+        await Task.Delay(4000);
+        Console.WriteLine("Eggs Done");
+    }
+
+    public static async Task CookChicken()
+    {
+        Console.WriteLine("Cooking Chicken");
+        await Task.Delay(5000);
+        Console.WriteLine("Chicken Done");
+    }
+
+    public static async Task MakeToast()
+    {
+        Console.WriteLine("Making Toast");
+        await Task.Delay(3000);
+        Console.WriteLine("Toast Done");
+    }
+
+}
---
+++ Multithreading/Program.cs
@@ -0,0 +1,100 @@
+﻿// Multithreading (To take advantage of all the cores of the CPU, to make it run fast)
+class Program
+{
+
+    private static int resourceAmount;
+    private static object lockObject = new object(); // to handle race condition
+    public static void Main(string[] args)
+    {
+
+        // Thread workerThread = new Thread(new ThreadStart(PathFinding));
+        // workerThread.Start();
+        // Console.WriteLine("Main Thread Working...");
+        // workerThread.Join();
+        // Console.WriteLine("Main Thread Done...");
+
+        // But There is a problem of race conditions while making thread which causes inconsistencies in result
+
+        Thread[] threadArray = new Thread[10];
+        for (int i = 0; i < 10; i++)
+        {
+            threadArray[i] = new Thread(AddResources);
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            threadArray[i].Start();
+        }
+
+        for (int i = 0; i < 10; i++)
+        {
+            threadArray[i].Join();
+        }
+        // As all the threads try to access the same resource at same time so there is chance of lost to some of resources
+        Console.WriteLine("Resource Amount: " + resourceAmount);
+
+
+        // Deadlock Example
+        DeadlockExample(); //Progam will stuck by running this function because both locks are waiting for each other to release it but none of them does
+
+    }
+
+    public static void PathFinding()
+    {
+        Console.WriteLine("Calculating Path Finding");
+        Thread.Sleep(3000);
+        Console.WriteLine("Path Finding Done");
+    }
+
+    public static void AddResources()
+    {
+        lock (lockObject) // As the resource become when time of updating so chance to lost to some resources now settled
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                int tmp = resourceAmount;
+                tmp++;
+                resourceAmount = tmp;
+            }
+        }
+    }
+
+    // Deadlock Example
+
+    public static void DeadlockExample()
+    {
+        object lock1 = new object();
+        object lock2 = new object();
+
+        Thread thread1 = new Thread(() =>
+        {
+            lock (lock1)
+            {
+                Thread.Sleep(100);
+                lock (lock2)
+                {
+                    Console.WriteLine("Thread 1 got both locks");
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(() =>
+        {
+            lock (lock2)
+            {
+                Thread.Sleep(100);
+                lock (lock1)
+                {
+                    Console.WriteLine("Thread 2 got both locks");
+                }
+            }
+        });
+
+        thread1.Start();
+        thread2.Start();
+
+        thread1.Join();
+        thread2.Join();
+    }
+}
+
